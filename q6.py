c9bb47fbee243ec5f9db3b8bc630c4d11212cf95
Reihanehprogram <Reihanehzahiri917@gmail.com>
2024-08-09 21:19:31 +0330

    +soal6

---
+++ q6.py
@@ -0,0 +1,11 @@
+a=int(input("adad 1"))
+b=int(input("adad 2"))
+c=int(input("adad 3"))
+d=int(input("adad 4"))
+e=int(input("adad 5"))
+f= a + b + c + d + e
+if f %2 == 0 :
+    NEW =int(input("addad jadid"))
+    print (f + NEW)
+else: 
+    print(a+b+d+e)
